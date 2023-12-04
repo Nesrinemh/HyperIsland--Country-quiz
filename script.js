c3864a1077caf978b2a0b0b5b77087a9eee9458d
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2023-12-04 13:37:40 +0100

    check awnser country and capital

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const capitalEl = document.querySelector("#capital");
 const continentEl = document.querySelector("#continent");
 const submitBtn = document.querySelector("#submitBtn");
 
+
+//create local storage 
+//pointsystem, varible 
+
+
+
 // _____________________________________________________
 // functions
 // _____________________________________________________
@@ -14,52 +20,83 @@ async function getData() {
 	let url = "https://restcountries.com/v3.1/all";
 	const response = await fetch(url);
 	const data = await response.json();
-
+	
 	let index = Math.round(Math.random() * 250);
 	let getQuestion = data[index];
 	flagEl.src = getQuestion.flags.png;
 
+	
 	let awnsersObj = {
 		country: getQuestion.altSpellings,
 		capital: getQuestion.capital,
 		continent: getQuestion.continents,
 	};
-
+	
+	console.log(awnsersObj)
 	//calling the getData() function returns an object with the awnsers
 	return awnsersObj;
 }
+let answerObject = await getData();
 
 
 
-function makeArrayUppercase(arr) {
+function arrayUppercase(arr) {
 	let newArr = [];
-
+	
 	for (let i = 0; i < arr.length; i++) {
 		newArr.push(arr[i].toUpperCase())
 	}
-
+	
 	return newArr
 }
- 
 
 
 
-async function checkAnswers(awnserObject) {
-	let correctContinent = awnserObject.continent;
-	console.log(correctContinent.toUpperCase());
+async function checkAnswers(answerObject) {
+	let correctCountry = arrayUppercase(answerObject.country);
+	let correctCapital = arrayUppercase(answerObject.capital);
+	let correctContinent = arrayUppercase(answerObject.continent);
+
+	let countryInput = countryEl.value.toUpperCase();
+	let capitalInput = capitalEl.value.toUpperCase();
+	let continentInput = continentEl.value.toUpperCase();
+
+
+
+	if(correctCountry.includes(countryInput)){
+		console.log('yaaaaaaay')
+		//adds a point to varieble 
+	}else{
+		console.log('noo :(')
+	}
+	if(correctCapital.includes(capitalInput)){
+		console.log('yaaaaaaay')
+		//adds a point to varieble 
+	}else{
+		console.log('noo :(')
+	}
+	if(correctContinent.includes(continentInput)){
+		console.log('yaaaaaaay')
+		//adds a point to varieble 
+	}else{
+		console.log('noo :(')
+	}
 }
+
 
 // _____________________________________________________
 // Event listners
 // _____________________________________________________
-submitBtn.addEventListener("click", async function () {
-	// put the function into a varieble like we said, we can now reach the object answers like this --> awnserObject.country[1] (just like in the check answers)
-	//its basically doing this getData().country
-	let awnserObject = await getData();
+submitBtn.addEventListener("click", async function (e) {
+	e.preventDefault()
 
-	//pass that object in to the check awnsers parameter so that we can use the object inside the function
-	checkAnswers(awnserObject);
+	checkAnswers(answerObject);
+	answerObject = await getData();
 });
+
+
+
+
 
 // timer
 let seconds = 59;
@@ -79,3 +116,7 @@ function stopTimer() {
 }
 
 // how to fetch array and object at the same time. and to call loop of object
+
+
+//if timer runs out, display score modal, store name + point in local storage and append to scoreboard
+//Sort scoreboard 
