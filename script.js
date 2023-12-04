c8d3930228b001f895a312d7f60d8e5f33dd3ed4
Nesrinemh <nesrine.hadboun@live.fr>
2023-12-04 12:50:56 +0100

    update

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-
+// _____________________________________________________
+// State variables
+// _____________________________________________________
 let flagEl = document.querySelector('#flag');
 const countryEl = document.querySelector('#country');
 const capitalEl = document.querySelector('#capital');
 const continentEl = document.querySelector('#continent');
-
 const submitBtn = document.querySelector('#submitBtn');
-submitBtn.addEventListener('click', function () {
-  checkAnswers();
-  getData();
-});
 
-async function getData(x) {
+// _____________________________________________________
+// functions
+// _____________________________________________________
+async function getData() {
   let url = 'https://restcountries.com/v3.1/all';
   const response = await fetch(url);
   const data = await response.json();
 
-	let index = Math.round(Math.random() * 250);
-	let getQuestion = data[index];
-	flagEl.src = getQuestion.flags.png;
+  let index = Math.round(Math.random() * 250);
+  let getQuestion = data[index];
+  flagEl.src = getQuestion.flags.png;
 
-	let awnsersObj = {
-		country: getQuestion.altSpellings,
-		capital: getQuestion.capital,
-		continent: getQuestion.continents,
-	};
+  let awnsersObj = {
+    country: getQuestion.altSpellings,
+    capital: getQuestion.capital,
+    continent: getQuestion.continents,
+  };
 
-	//calling the getData() function returns an object with the awnsers
-	return awnsersObj;
+  //calling the getData() function returns an object with the awnsers
+  return awnsersObj;
+}
+
+function makeArrayUppercase(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
+  }
+
+  return newArr;
 }
 
 async function checkAnswers(awnserObject) {
-	//temporary logs to show how it works :) 
-	console.log(awnserObject.country);
-	console.log(awnserObject.country[1]);
-	
-	console.log(awnserObject.capital);
-	console.log(awnserObject.continent);
+  let correctContinent = awnserObject.continent;
+  console.log(correctContinent.toUpperCase());
 }
 
 // _____________________________________________________
 // Event listners
 // _____________________________________________________
-submitBtn.addEventListener("click", async function () {
-	// put the function into a varieble like we said, we can now reach the object answers like this --> awnserObject.country[1] (just like in the check answers)
-	//its basically doing this getData().country 
-	let awnserObject = await getData();
+submitBtn.addEventListener('click', async function () {
+  // put the function into a varieble like we said, we can now reach the object answers like this --> awnserObject.country[1] (just like in the check answers)
+  //its basically doing this getData().country
+  let awnserObject = await getData();
 
-	//pass that object in to the check awnsers parameter so that we can use the object inside the function
-	checkAnswers(awnserObject);
+  //pass that object in to the check awnsers parameter so that we can use the object inside the function
+  checkAnswers(awnserObject);
 });
 
-
-
-
-
-
-
-// timer
+// _____________________________________________________
+// Timer & Modal
+// _____________________________________________________
 let seconds = 59;
 const modal = document.getElementById('myModal');
 const close = document.getElementsByClassName('close')[0];
@@ -76,7 +78,6 @@ modaleBtnEl.onclick = function () {
   modal.style.display = 'none';
   window.location.href = 'index.html';
 };
-
 
 // how to fetch array and object at the same time. and to call loop of object
 
