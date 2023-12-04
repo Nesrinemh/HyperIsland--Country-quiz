74b44a15c2fcca21d9feb6adf4c6d2abf474549b
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2023-12-04 11:43:18 +0100

    function that makes all array indexes to uppercase

--- script.js
+++ script.js
@@ -29,13 +29,24 @@ async function getData() {
 	return awnsersObj;
 }
 
+
+
+function makeArrayUppercase(arr) {
+	let newArr = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		newArr.push(arr[i].toUpperCase())
+	}
+
+	return newArr
+}
+ 
+
+
+
 async function checkAnswers(awnserObject) {
-	//temporary logs to show how it works :) 
-	console.log(awnserObject.country);
-	console.log(awnserObject.country[1]);
-	
-	console.log(awnserObject.capital);
-	console.log(awnserObject.continent);
+	let correctContinent = awnserObject.continent;
+	console.log(correctContinent.toUpperCase());
 }
 
 // _____________________________________________________
@@ -43,34 +54,28 @@ async function checkAnswers(awnserObject) {
 // _____________________________________________________
 submitBtn.addEventListener("click", async function () {
 	// put the function into a varieble like we said, we can now reach the object answers like this --> awnserObject.country[1] (just like in the check answers)
-	//its basically doing this getData().country 
+	//its basically doing this getData().country
 	let awnserObject = await getData();
 
 	//pass that object in to the check awnsers parameter so that we can use the object inside the function
 	checkAnswers(awnserObject);
 });
 
-
-
-
-
-
-
 // timer
 let seconds = 59;
 
 let interval = setInterval(function () {
-  seconds -= 1;
+	seconds -= 1;
 
-  let timerEl = document.querySelector("#timer");
-  timerEl.textContent = seconds;
-  if (seconds == 0) {
-    stopTimer();
-  }
+	let timerEl = document.querySelector("#timer");
+	timerEl.textContent = seconds;
+	if (seconds == 0) {
+		stopTimer();
+	}
 }, 1000);
 
 function stopTimer() {
-  clearInterval(interval);
+	clearInterval(interval);
 }
 
 // how to fetch array and object at the same time. and to call loop of object
