b0a6428eae52772bbac33dce0a5d85e368645970
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2023-12-04 10:51:16 +0100

    commenting

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// _____________________________________________________
+// State variables
+// _____________________________________________________
 let flagEl = document.querySelector("#flag");
 const countryEl = document.querySelector("#country");
 const capitalEl = document.querySelector("#capital");
 const continentEl = document.querySelector("#continent");
-
 const submitBtn = document.querySelector("#submitBtn");
-submitBtn.addEventListener("click", async function () {
-  let awnserObject = await getData();
-	checkAnswers(awnserObject);
-});
 
+// _____________________________________________________
+// functions
+// _____________________________________________________
 async function getData() {
 	let url = "https://restcountries.com/v3.1/all";
 	const response = await fetch(url);
@@ -19,16 +20,33 @@ async function getData() {
 	flagEl.src = getQuestion.flags.png;
 
 	let awnsersObj = {
-		countryAnswer: getQuestion.altSpellings,
-		capitalAnswer: getQuestion.capital,
-		continentAnswer: getQuestion.continents,
+		country: getQuestion.altSpellings,
+		capital: getQuestion.capital,
+		continent: getQuestion.continents,
 	};
 
+	//calling the getData() function returns an object with the awnsers
 	return awnsersObj;
 }
 
-async function checkAnswers(awnserObject){
-  console.log(awnserObject.capitalAnswer)
+async function checkAnswers(awnserObject) {
+	console.log(awnserObject.capital);
 }
+
+// _____________________________________________________
+// Event listners
+// _____________________________________________________
+submitBtn.addEventListener("click", async function () {
+	// api function in varieble (the varieble is the object)
+	let awnserObject = await getData();
+	//pass that object in to the check awnsers
+	checkAnswers(awnserObject);
+});
+
+
+
+
+
+
 
 // how to fetch array and object at the same time. and to call loop of object
