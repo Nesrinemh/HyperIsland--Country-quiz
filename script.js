07d06616b8ea67f8d80d28e3cc33e6276381e8f8
Nesrinemh <nesrine.hadboun@live.fr>
2023-12-04 17:52:17 +0100

    change country fetch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // _____________________________________________________
 // State variables
 // _____________________________________________________
-let flagEl = document.querySelector("#flag");
-const countryEl = document.querySelector("#country");
-const capitalEl = document.querySelector("#capital");
-const continentEl = document.querySelector("#continent");
-const submitBtn = document.querySelector("#submitBtn");
+let flagEl = document.querySelector('#flag');
+const countryEl = document.querySelector('#country');
+const capitalEl = document.querySelector('#capital');
+const continentEl = document.querySelector('#continent');
+const submitBtn = document.querySelector('#submitBtn');
 
 //create local storage
 //pointsystem, varible
@@ -14,83 +14,83 @@ const submitBtn = document.querySelector("#submitBtn");
 // functions
 // _____________________________________________________
 async function getData() {
-	let url = "https://restcountries.com/v3.1/all";
-	const response = await fetch(url);
-	const data = await response.json();
+  let url = 'https://restcountries.com/v3.1/all';
+  const response = await fetch(url);
+  const data = await response.json();
 
-	let index = Math.round(Math.random() * 250);
-	let getQuestion = data[index];
-	flagEl.src = getQuestion.flags.png;
+  let index = Math.round(Math.random() * 250);
+  let getQuestion = data[index];
+  flagEl.src = getQuestion.flags.png;
 
-	let awnsersObj = {
-		country: getQuestion.altSpellings,
-		capital: getQuestion.capital,
-		continent: getQuestion.continents,
-	};
+  let awnsersObj = {
+    country: getQuestion.name.common,
+    capital: getQuestion.capital,
+    continent: getQuestion.continents,
+  };
 
-	//calling the getData() function returns an object with the awnsers
-	return awnsersObj;
+  //calling the getData() function returns an object with the awnsers
+  return awnsersObj;
 }
 
 function makeArrayUppercase(arr) {
-	let newArr = [];
+  let newArr = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i].toUpperCase());
-	}
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
+  }
 
-	return newArr;
+  return newArr;
 }
 let answerObject = await getData();
 
 function arrayUppercase(arr) {
-	let newArr = [];
+  let newArr = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i].toUpperCase());
-	}
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
+  }
 
-	return newArr;
+  return newArr;
 }
 
 async function checkAnswers(answerObject) {
-	let correctCountry = arrayUppercase(answerObject.country);
-	let correctCapital = arrayUppercase(answerObject.capital);
-	let correctContinent = arrayUppercase(answerObject.continent);
+  let correctCountry = arrayUppercase(answerObject.country);
+  let correctCapital = arrayUppercase(answerObject.capital);
+  let correctContinent = arrayUppercase(answerObject.continent);
 
-	let countryInput = countryEl.value.toUpperCase();
-	let capitalInput = capitalEl.value.toUpperCase();
-	let continentInput = continentEl.value.toUpperCase();
+  let countryInput = countryEl.value.toUpperCase();
+  let capitalInput = capitalEl.value.toUpperCase();
+  let continentInput = continentEl.value.toUpperCase();
 
-	if (correctCountry.includes(countryInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
-	if (correctCapital.includes(capitalInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
-	if (correctContinent.includes(continentInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
+  if (correctCountry.includes(countryInput)) {
+    console.log('yaaaaaaay');
+    //adds a point to varieble
+  } else {
+    console.log('noo :(');
+  }
+  if (correctCapital.includes(capitalInput)) {
+    console.log('yaaaaaaay');
+    //adds a point to varieble
+  } else {
+    console.log('noo :(');
+  }
+  if (correctContinent.includes(continentInput)) {
+    console.log('yaaaaaaay');
+    //adds a point to varieble
+  } else {
+    console.log('noo :(');
+  }
 }
 
 // _____________________________________________________
 // Event listners
 // _____________________________________________________
 
-submitBtn.addEventListener("click", async function (e) {
-	e.preventDefault();
+submitBtn.addEventListener('click', async function (e) {
+  e.preventDefault();
 
-	checkAnswers(answerObject);
-	answerObject = await getData();
+  checkAnswers(answerObject);
+  answerObject = await getData();
 });
 
 // _____________________________________________________
@@ -98,23 +98,23 @@ submitBtn.addEventListener("click", async function (e) {
 // _____________________________________________________
 
 let seconds = 59;
-const modal = document.getElementById("myModal");
-const close = document.getElementsByClassName("close")[0];
-const modaleBtnEl = document.getElementsByClassName("modal-btn")[0];
+const modal = document.getElementById('myModal');
+const close = document.getElementsByClassName('close')[0];
+const modaleBtnEl = document.getElementsByClassName('modal-btn')[0];
 let interval = setInterval(function () {
-	seconds -= 1;
+  seconds -= 1;
 
-	let timerEl = document.querySelector("#timer");
-	timerEl.textContent = seconds;
-	if (seconds === 0) {
-		stopTimer();
-		console.log("hello");
-		modal.style.display = "block";
-	}
+  let timerEl = document.querySelector('#timer');
+  timerEl.textContent = seconds;
+  if (seconds === 0) {
+    stopTimer();
+    console.log('hello');
+    modal.style.display = 'block';
+  }
 }, 1000);
 
 function stopTimer() {
-	clearInterval(interval);
+  clearInterval(interval);
 }
 
 // how to fetch array and object at the same time. and to call loop of object
@@ -123,8 +123,8 @@ function stopTimer() {
 //Sort scoreboard
 
 modaleBtnEl.onclick = function () {
-	modal.style.display = "none";
-	window.location.href = "index.html";
+  modal.style.display = 'none';
+  window.location.href = 'index.html';
 };
 
 // how to fetch array and object at the same time. and to call loop of object
