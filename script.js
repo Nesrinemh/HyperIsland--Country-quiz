ce0f198f11bd07b177221836eb893c20d98d601a
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2023-12-04 10:19:37 +0100

    refractor api

--- script.js
+++ script.js
@@ -5,32 +5,27 @@ const continentEl = document.querySelector("#continent");
 
 const submitBtn = document.querySelector("#submitBtn");
 submitBtn.addEventListener("click", function () {
-  checkAnswers();
-  getData();
+	checkAnswers();
+	getData(0);
 });
 
 async function getData(x) {
-  let url = "https://restcountries.com/v3.1/all";
-  const response = await fetch(url);
-  const data = await response.json();
+	let url = "https://restcountries.com/v3.1/all";
+	const response = await fetch(url);
+	const data = await response.json();
 
-  let index = Math.round(Math.random() * 250);
-  let getQuestion = data[index];
-  console.log(getQuestion);
-  flagEl.src = getQuestion.flags.png;
+	let index = Math.round(Math.random() * 250);
+	let getQuestion = data[index];
+	flagEl.src = getQuestion.flags.png;
 
-  let countryAnswer = getQuestion.altSpellings;
-  let capitalAnswer = getQuestion.capital;
-  let continentAnswer = getQuestion.continents;
+	let answers = [getQuestion.altSpellings, getQuestion.capital, getQuestion.continents];
 
-  let answers = [countryAnswer, capitalAnswer, continentAnswer];
-
-  return answers[x];
+	console.log(answers[x]);
 }
 getData();
 
 async function checkAnswers() {
-  console.log(await getData([0]));
+	
 }
 
 // how to fetch array and object at the same time. and to call loop of object
