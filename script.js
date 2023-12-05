283880e2625885c18bf07f94f5e603804b55b23f
minimoocodes <jaemin.shim14@gmail.com>
2023-12-05 11:11:45 +0100

    typo fixed

--- script.js
+++ script.js
@@ -14,72 +14,72 @@ const submitBtn = document.querySelector("#submitBtn");
 // functions
 // _____________________________________________________
 async function getData() {
-	let url = "https://restcountries.com/v3.1/all";
-	const response = await fetch(url);
-	const data = await response.json();
+  let url = "https://restcountries.com/v3.1/all";
+  const response = await fetch(url);
+  const data = await response.json();
 
-	let index = Math.round(Math.random() * 250);
-	let getQuestion = data[index];
-	flagEl.src = getQuestion.flags.png;
+  let index = Math.round(Math.random() * 250);
+  let getQuestion = data[index];
+  flagEl.src = getQuestion.flags.png;
 
-	let awnsersObj = {
-		country: getQuestion.altSpellings,
-		capital: getQuestion.capital,
-		continent: getQuestion.continents,
-	};
+  let awnsersObj = {
+    country: getQuestion.altSpellings,
+    capital: getQuestion.capital,
+    continent: getQuestion.continents,
+  };
 
-	//calling the getData() function returns an object with the awnsers
-	return awnsersObj;
+  //calling the getData() function returns an object with the awnsers
+  return awnsersObj;
 }
 
 function makeArrayUppercase(arr) {
-	let newArr = [];
+  let newArr = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i].toUpperCase());
-	}
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
+  }
 
-	return newArr;
+  return newArr;
 }
 let answerObject = await getData();
 
 function arrayUppercase(arr) {
-	let newArr = [];
+  let newArr = [];
 
-	for (let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i].toUpperCase());
-	}
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].toUpperCase());
+  }
 
-	return newArr;
+  return newArr;
 }
 
 async function checkAnswers(answerObject) {
-	let correctCountry = arrayUppercase(answerObject.country);
-	let correctCapital = arrayUppercase(answerObject.capital);
-	let correctContinent = arrayUppercase(answerObject.continent);
+  let correctCountry = arrayUppercase(answerObject.country);
+  let correctCapital = arrayUppercase(answerObject.capital);
+  let correctContinent = arrayUppercase(answerObject.continent);
 
-	let countryInput = countryEl.value.toUpperCase();
-	let capitalInput = capitalEl.value.toUpperCase();
-	let continentInput = continentEl.value.toUpperCase();
+  let countryInput = countryEl.value.toUpperCase();
+  let capitalInput = capitalEl.value.toUpperCase();
+  let continentInput = continentEl.value.toUpperCase();
 
-	if (correctCountry.includes(countryInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
-	if (correctCapital.includes(capitalInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
-	if (correctContinent.includes(continentInput)) {
-		console.log("yaaaaaaay");
-		//adds a point to varieble
-	} else {
-		console.log("noo :(");
-	}
+  if (correctCountry.includes(countryInput)) {
+    console.log("yaaaaaaay");
+    //adds a point to varieble
+  } else {
+    console.log("noo :(");
+  }
+  if (correctCapital.includes(capitalInput)) {
+    console.log("yaaaaaaay");
+    //adds a point to varieble
+  } else {
+    console.log("noo :(");
+  }
+  if (correctContinent.includes(continentInput)) {
+    console.log("yaaaaaaay");
+    //adds a point to varieble
+  } else {
+    console.log("noo :(");
+  }
 }
 
 // _____________________________________________________
@@ -87,10 +87,10 @@ async function checkAnswers(answerObject) {
 // _____________________________________________________
 
 submitBtn.addEventListener("click", async function (e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	checkAnswers(answerObject);
-	answerObject = await getData();
+  checkAnswers(answerObject);
+  answerObject = await getData();
 });
 
 // _____________________________________________________
@@ -102,19 +102,19 @@ const modal = document.getElementById("myModal");
 const close = document.getElementsByClassName("close")[0];
 const modaleBtnEl = document.getElementsByClassName("modal-btn")[0];
 let interval = setInterval(function () {
-	seconds -= 1;
+  seconds -= 1;
 
-	let timerEl = document.querySelector("#timer");
-	timerEl.textContent = seconds;
-	if (seconds === 0) {
-		stopTimer();
-		console.log("hello");
-		modal.style.display = "block";
-	}
+  let timerEl = document.querySelector("#timer");
+  timerEl.textContent = seconds;
+  if (seconds === 0) {
+    stopTimer();
+    console.log("hello");
+    modal.style.display = "block";
+  }
 }, 1000);
 
 function stopTimer() {
-	clearInterval(interval);
+  clearInterval(interval);
 }
 
 // how to fetch array and object at the same time. and to call loop of object
@@ -123,8 +123,8 @@ function stopTimer() {
 //Sort scoreboard
 
 modaleBtnEl.onclick = function () {
-	modal.style.display = "none";
-	window.location.href = "index.html";
+  modal.style.display = "none";
+  window.location.href = "index.html";
 };
 
 // how to fetch array and object at the same time. and to call loop of object
