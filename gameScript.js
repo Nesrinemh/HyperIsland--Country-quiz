bfdc04cc831dddbdeadf7d5017a244f51ec4d023
minimoocodes <jaemin.shim14@gmail.com>
2023-12-08 12:34:52 +0100

    sound effect

--- gameScript.js
+++ gameScript.js
@@ -95,7 +95,7 @@ submitBtnEl.addEventListener("click", async function (e) {
 // Timer & Modal
 // _____________________________________________________
 
-let seconds = 60;
+let seconds = 10;
 const modal = document.getElementById("myModal");
 const modalBtnEl = document.getElementsByClassName("modal-btn")[0];
 
